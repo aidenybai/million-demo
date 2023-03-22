53ee4f6c7d86e3f240a637dcf107782045c0c19d
Aiden Bai <aiden.bai05@gmail.com>
2023-03-22 01:50:09 -0700

    feat: github corner

--- src/app.jsx
+++ src/app.jsx
@@ -18,6 +18,7 @@ import {
   Text,
 } from '@chakra-ui/react';
 import { startTransition, useState } from 'react';
+import GithubCorner from 'react-github-corner';
 import TimesTable from './times-table';
 
 function App() {
@@ -28,71 +29,79 @@ function App() {
     });
   };
   return (
-    <Container p={8}>
-      <Stack direction="column" spacing={3}>
-        <Heading>Million vs React Demo</Heading>
-        <Text>
-          The following is a random times table generator benchmark (
-          <Link href="https://github.com/aidenybai/million-demo" isExternal>
-            source <ExternalLinkIcon mx="2px" />
-          </Link>
-          ). It follows a comparison between React and Million, along with its
-          fiber equivalents.
-        </Text>
-        <Text>
-          In order to invoke a re-render, click the "Increment "button below.
-          The "Lag" radar indicates blocking time, but does not account for the
-          time it takes to update the UI. Notice the time it takes to update the
-          UI after you click to get an idea for update time.
-        </Text>
-        <Text fontStyle="italic" size="sm">
-          Caveat to note: Every row contains 100 empty <Code>{'<div />'}</Code>{' '}
-          nodes to stimulate diffing in order to measure performance.
-        </Text>
-        <Text>
-          You can adjust the number of rows by using the the number input. Make
-          sure to adjust if you can't see any performance difference or your
-          screen freezes.
-        </Text>
-      </Stack>
+    <>
+      <Container p={8}>
+        <Stack direction="column" spacing={3}>
+          <Heading>Million vs React Demo</Heading>
+          <Text>
+            The following is a random times table generator benchmark (
+            <Link
+              href="https://github.com/aidenybai/million-demo"
+              isExternal
+              color="purple"
+            >
+              source <ExternalLinkIcon mx="2px" />
+            </Link>
+            ). It follows a comparison between React and Million, along with its
+            fiber equivalents.
+          </Text>
+          <Text>
+            In order to invoke a re-render, click the "Increment "button below.
+            The "Lag" radar indicates blocking time, but does not account for
+            the time it takes to update the UI. Notice the time it takes to
+            update the UI after you click to get an idea for update time.
+          </Text>
+          <Text fontStyle="italic" size="sm">
+            Caveat to note: Every row contains 100 empty{' '}
+            <Code>{'<div />'}</Code> nodes to stimulate diffing in order to
+            measure performance.
+          </Text>
+          <Text>
+            You can adjust the number of rows by using the the number input.
+            Make sure to adjust if you can't see any performance difference or
+            your screen freezes.
+          </Text>
+        </Stack>
 
-      <NumberInput
-        mt={3}
-        maxW={1000}
-        min={0}
-        mr="2rem"
-        value={nodes}
-        onChange={handleChange}
-      >
-        <NumberInputField placeholder="Enter number of rows" />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
-      <Tabs isLazy={true} variant="soft-rounded" colorScheme="purple" mt={3}>
-        <TabList>
-          <Tab>React</Tab>
-          <Tab>React Fiber</Tab>
-          <Tab>⚡ Million</Tab>
-          <Tab>Million Fiber</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <TimesTable nodes={nodes} mode="react" />
-          </TabPanel>
-          <TabPanel>
-            <TimesTable nodes={nodes} mode="react-fiber" />
-          </TabPanel>
-          <TabPanel>
-            <TimesTable nodes={nodes} mode="million" />
-          </TabPanel>
-          <TabPanel>
-            <TimesTable nodes={nodes} mode="million-fiber" />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Container>
+        <NumberInput
+          mt={3}
+          maxW={1000}
+          min={0}
+          mr="2rem"
+          value={nodes}
+          onChange={handleChange}
+        >
+          <NumberInputField placeholder="Enter number of rows" />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+        <Tabs isLazy={true} variant="soft-rounded" colorScheme="purple" mt={3}>
+          <TabList>
+            <Tab>React</Tab>
+            <Tab>React Fiber</Tab>
+            <Tab>⚡ Million</Tab>
+            <Tab>Million Fiber</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <TimesTable nodes={nodes} mode="react" />
+            </TabPanel>
+            <TabPanel>
+              <TimesTable nodes={nodes} mode="react-fiber" />
+            </TabPanel>
+            <TabPanel>
+              <TimesTable nodes={nodes} mode="million" />
+            </TabPanel>
+            <TabPanel>
+              <TimesTable nodes={nodes} mode="million-fiber" />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Container>
+      <GithubCorner href="https://github.com/aidenybai/million" />
+    </>
   );
 }
 
