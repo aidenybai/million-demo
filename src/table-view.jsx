afb20a1ed1d2cc6972da4772733b6c06127075d2
Aiden Bai <aiden.bai05@gmail.com>
2023-03-22 03:31:03 -0700

    feat: use suspense

---
+++ src/table-view.jsx
@@ -0,0 +1,74 @@
+import { Table, TableContainer, Tbody, Th, Thead, Tr } from '@chakra-ui/react';
+import { block, For } from 'million/react';
+
+function Row({ product, count, random }) {
+  return (
+    <tr>
+      <td>
+        <code>{random}</code>
+      </td>
+      <td>
+        <code>{count}</code>
+      </td>
+      <td>
+        <code>{product}</code>
+      </td>
+      <td>
+        <code>
+          {random} * {count} = {product}
+        </code>
+      </td>
+      <div>
+        {Array(100)
+          .fill(0)
+          .map(() => (
+            <div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </tr>
+  );
+}
+
+const RowBlock = block(Row);
+
+function TableView({ array, count, mode }) {
+  return (
+    <TableContainer>
+      <Table size="md">
+        <Thead>
+          <Tr>
+            <Th>Random</Th>
+            <Th>Count</Th>
+            <Th>Product</Th>
+            <Th>Equation</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {mode === 'million' ? (
+            <For each={array}>
+              {(random) => {
+                return (
+                  <RowBlock
+                    product={count * random}
+                    random={random}
+                    count={count}
+                  />
+                );
+              }}
+            </For>
+          ) : (
+            array.map((random) => (
+              <Row product={count * random} random={random} count={count} />
+            ))
+          )}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default TableView;
--- src/times-table.jsx
+++ src/times-table.jsx
@@ -1,80 +1,35 @@
 import {
   Button,
   Flex,
-  Stack,
-  Table,
-  TableContainer,
-  Tbody,
-  Text,
-  Th,
-  Thead,
-  Tr,
+  SkeletonCircle,
   SkeletonText,
+  Stack,
+  Text,
 } from '@chakra-ui/react';
-import { block, For } from 'million/react';
-import { startTransition, useEffect, useState } from 'react';
-import LagRadar from 'react-lag-radar';
-
-function Row({ product, count, random }) {
-  return (
-    <tr>
-      <td>
-        <code>{random}</code>
-      </td>
-      <td>
-        <code>{count}</code>
-      </td>
-      <td>
-        <code>{product}</code>
-      </td>
-      <td>
-        <code>
-          {random} * {count} = {product}
-        </code>
-      </td>
-      <div>
-        {Array(100)
-          .fill(0)
-          .map(() => (
-            <div>
-              <div>
-                <div></div>
-              </div>
-            </div>
-          ))}
-      </div>
-    </tr>
-  );
-}
-
-const RowBlock = block(Row);
+import { startTransition, Suspense, useState, lazy } from 'react';
+const LagRadar = lazy(() => import('react-lag-radar'));
+const TableView = lazy(() => import('./table-view'));
 
 const descriptions = {
   react: 'High blocking time, but low delay in updating UI',
   'react-fiber': 'Low blocking time, but high delay in updating UI',
   million: 'Low blocking time, and low delay in updating UI',
-  'million-fiber': 'Low blocking time, and high delay in updating UI',
 };
-
-const globalArray = Array(1000);
-for (let i = 0; i < 1000; i++) {
-  globalArray[i] = Math.floor(Math.random() * 10);
-}
 
 const TimesTable = ({ nodes, mode }) => {
   const [count, setCount] = useState(0);
-  const [array, setArray] = useState([]);
 
-  useEffect(() => {
-    startTransition(() => {
-      setArray(globalArray.slice(0, nodes));
-    });
-  }, []);
+  const array = Array(nodes);
+  for (let i = 0; i < nodes; i++) {
+    array[i] = Math.floor(Math.random() * 100);
+  }
 
   return (
     <Stack direction="column" spacing={5}>
       <Flex justifyContent="center" alignItems="center" flexDirection="column">
-        <LagRadar size={200} />
+        <Suspense fallback={<SkeletonCircle size="200" />}>
+          <LagRadar size={200} />
+        </Suspense>
         <Text fontSize="xs" mb={3}>
           {descriptions[mode]}
         </Text>
@@ -96,42 +51,11 @@ const TimesTable = ({ nodes, mode }) => {
         <Text fontSize="md">Increment ({count})</Text>
       </Button>
 
-      <SkeletonText
-        isLoaded={array.length !== 0}
-        noOfLines={9}
-        spacing="3"
-        skeletonHeight="4"
+      <Suspense
+        fallback={<SkeletonText noOfLines={9} spacing="3" skeletonHeight="4" />}
       >
-        <TableContainer>
-          <Table size="md">
-            <Thead>
-              <Tr>
-                <Th>Random</Th>
-                <Th>Count</Th>
-                <Th>Product</Th>
-                <Th>Equation</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {mode === 'million' ? (
-                <For each={array}>
-                  {(random) => (
-                    <RowBlock
-                      product={count * random}
-                      random={random}
-                      count={count}
-                    />
-                  )}
-                </For>
-              ) : (
-                array.map((random) => (
-                  <Row product={count * random} random={random} count={count} />
-                ))
-              )}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </SkeletonText>
+        <TableView array={array} count={count} mode={mode} />
+      </Suspense>
     </Stack>
   );
 };
