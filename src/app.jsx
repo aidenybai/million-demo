44c9f04be05ac0a6c4b113a6628419b405a192c2
Aiden Bai <aiden.bai05@gmail.com>
2023-03-22 02:34:03 -0700

    feat: use modal

--- src/app.jsx
+++ src/app.jsx
@@ -16,23 +16,118 @@ import {
   TabPanels,
   Tabs,
   Text,
+  UnorderedList,
+  ListItem,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
 } from '@chakra-ui/react';
-import { startTransition, useState } from 'react';
+import { startTransition, useEffect, useState } from 'react';
 import GithubCorner from 'react-github-corner';
 import TimesTable from './times-table';
 
 function App() {
   const [nodes, setNodes] = useState(1000);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const handleChange = (nodes) => {
     startTransition(() => {
       setNodes(nodes);
     });
   };
+  useEffect(() => {
+    onOpen();
+  }, []);
   return (
     <>
+      <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Million vs. React Demo</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Stack direction="column" spacing={3}>
+              <Text>
+                The following is a random times table generator benchmark (
+                <Link
+                  href="https://github.com/aidenybai/million-demo"
+                  isExternal
+                  color="purple"
+                >
+                  source <ExternalLinkIcon mx="2px" />
+                </Link>
+                ). It follows a comparison between React and Million, along with
+                its fiber equivalents.
+              </Text>
+              <Heading as="h4" size="md">
+                Instructions
+              </Heading>
+              <Text>
+                In order to invoke a re-render, click the "Increment "button
+                below. The "Lag" radar indicates blocking time, but does not
+                account for the time it takes to update the UI. Notice the time
+                it takes to update the UI after you click to get an idea for
+                update time.
+              </Text>
+              <Heading as="h4" size="md">
+                Notes
+              </Heading>
+              <Text>
+                <UnorderedList>
+                  <ListItem>
+                    All implementations are within React. The only difference
+                    with Million implementation is a{' '}
+                    <Link
+                      href="https://github.com/aidenybai/million-demo/blob/53ee4f6c7d86e3f240a637dcf107782045c0c19d/src/times-table.jsx#L49"
+                      isExternal
+                      color="purple"
+                    >
+                      <Code>{'block()'}</Code> wrapper
+                    </Link>{' '}
+                    and the{' '}
+                    <Link
+                      href="https://github.com/aidenybai/million-demo/blob/53ee4f6c7d86e3f240a637dcf107782045c0c19d/src/times-table.jsx#L104"
+                      isExternal
+                      color="purple"
+                    >
+                      <Code>{'<For />'}</Code> component
+                    </Link>
+                    .
+                  </ListItem>
+                  <ListItem>
+                    Every row contains 100 empty <Code>{'<div />'}</Code> nodes
+                    to stimulate diffing in order to measure performance.
+                  </ListItem>
+                  <ListItem>
+                    Should not be used as a benchmark for real-world
+                    applications (this is a very intensive case that isn't
+                    necessarily representative)
+                  </ListItem>
+                </UnorderedList>
+              </Text>
+              <Text>
+                You can adjust the number of rows by using the the number input.
+                Make sure to adjust if you can't see any performance difference
+                or your screen freezes.
+              </Text>
+            </Stack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="purple" mr={3} onClick={onClose}>
+              Okay!
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
       <Container p={8}>
         <Stack direction="column" spacing={3}>
-          <Heading>Million vs React Demo</Heading>
+          <Heading>Million vs. React Demo</Heading>
           <Text>
             The following is a random times table generator benchmark (
             <Link
@@ -44,22 +139,6 @@ function App() {
             </Link>
             ). It follows a comparison between React and Million, along with its
             fiber equivalents.
-          </Text>
-          <Text>
-            In order to invoke a re-render, click the "Increment "button below.
-            The "Lag" radar indicates blocking time, but does not account for
-            the time it takes to update the UI. Notice the time it takes to
-            update the UI after you click to get an idea for update time.
-          </Text>
-          <Text fontStyle="italic" size="sm">
-            Caveat to note: Every row contains 100 empty{' '}
-            <Code>{'<div />'}</Code> nodes to stimulate diffing in order to
-            measure performance.
-          </Text>
-          <Text>
-            You can adjust the number of rows by using the the number input.
-            Make sure to adjust if you can't see any performance difference or
-            your screen freezes.
           </Text>
         </Stack>
 
